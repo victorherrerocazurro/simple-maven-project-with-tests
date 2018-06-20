e6c939132129075aaf29fd0d72801884040525e0
Victor Herrero Cazurro <victorherrerocazurro@gmail.com>
2018-06-20 18:29:50 +0200

    Update Base.java
    Arreglamos el fallo

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,12 +30,12 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
+        /*double r = Math.random();
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
-        }
+        }*/
     }
 
 }
